c66a992de975a52f9f5f7285fd13d23e75e24582
july2m3 <july2m3@gmail.com>
2020-06-15 15:23:08 -0500

    Added layout for gourmet restaurant example.

---
+++ src/components/gourmet-catering/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function GourmetCatering() {
+    return (
+        <div>
+            {/* nav, header, about, menu, contact, footer */}
+            
+        </div>
+    )
+}
+
+export default GourmetCatering ;
