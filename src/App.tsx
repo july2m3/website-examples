941ea1d95b64700465d6727b52dd5cccdb1dd57e
july2m3 <july2m3@gmail.com>
2020-06-17 13:27:15 -0500

    Replaced links with buttons

--- src/App.tsx
+++ src/App.tsx
@@ -12,60 +12,48 @@ const App = () => {
   return (
     <BrowserRouter>
       <>
-        <nav className='nav__top'>
+        <nav className="nav__top">
           <ul>
-            <li className='li__view-code'>
-              <a href='https://github.com/july2m3/website-examples'>
+            <li className="li__view-code">
+              <a href="https://github.com/july2m3/website-examples">
                 View Code
               </a>
             </li>
-            <li className='li__picture li__picture-left'>
-              <i className='fas fa-desktop nav__selection' />
+            <li className="li__picture li__picture-left">
+              <i className="fas fa-desktop nav__selection" />
             </li>
-            <li className='li__picture'>
-              <i className='fas fa-mobile-alt nav__selection' />
+            <li className="li__picture">
+              <i className="fas fa-mobile-alt nav__selection" />
             </li>
-            <li className='li__dropdown'>
-              <div className='dropdown'>
-                <button className='dropbtn'>
-                  Web-Site <i className='fas fa-angle-down nav__chevron'></i>
+            <li className="li__dropdown">
+              <div className="dropdown">
+                <button className="dropbtn">
+                  Web-Site <i className="fas fa-angle-down nav__chevron"></i>
                 </button>
-                <div className='dropdown-content'>
-                  <Link to='/'>
-                    <a href='#'>Home</a>
+                <div className="dropdown-content">
+                  <Link to="/">
+                    <button className="link">Home</button>
                   </Link>
-                  <Link to='/test'>
-                    <a href='#'>Test</a>
+                  <Link to="/test">
+                    <button className="link">Test</button>
                   </Link>
-                  <Link to='card-layout'>
-                    <a href='#'>Card Layout</a>
+                  <Link to="card-layout">
+                    <button className="link">Card Layout</button>
                   </Link>
                 </div>
               </div>
-              {/* <button className='button__dropdown'>Website Themes</button>
-              <div className='div_dropdown-content'>
-                <p>
-                  <Link to='/'>Home</Link>
-                </p>
-                <p>
-                  <Link to='/card-layout'>Card Layout</Link>
-                </p>
-                <p>
-                  <Link to='/test'>Test</Link>
-                </p>
-              </div> */}
             </li>
           </ul>
         </nav>
-        <main className='main__content'>
+        <main className="main__content">
           <Switch>
-            <Route path='/card-layout'>
+            <Route path="/card-layout">
               <CardLayout />
             </Route>
-            <Route path='/test'>
+            <Route path="/test">
               <Test />
             </Route>
-            <Route path='/'>
+            <Route path="/">
               <Home />
             </Route>
           </Switch>
