5199c9e87089703be1a36c21d3e7306257a0a88a
Julio <july2m3@gmail.com>
2020-06-22 10:12:21 -0500

    Updated title to match app

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # website-examples
 
 A repository for example websites with different layouts
+
+##Gourmet-Catering
+A fany restaurant
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 import Home from './components/home/Home';
-import GourmetCatering from './components/gourmet-catering/App';
+import GourmetCatering from './components/gourmet-catering/';
 
 import './App.css';
 
@@ -10,42 +10,42 @@ const App = () => {
   return (
     <BrowserRouter>
       <>
-        <nav className="nav__top">
+        <nav className='nav__top'>
           <ul>
-            <li className="li__view-code">
-              <a href="https://github.com/july2m3/website-examples">
+            <li className='li__view-code'>
+              <a href='https://github.com/july2m3/website-examples'>
                 View Code
               </a>
             </li>
-            <li className="li__picture li__picture-left">
-              <i className="fas fa-desktop nav__selection" />
+            <li className='li__picture li__picture-left'>
+              <i className='fas fa-desktop nav__selection' />
             </li>
-            <li className="li__picture">
-              <i className="fas fa-mobile-alt nav__selection" />
+            <li className='li__picture'>
+              <i className='fas fa-mobile-alt nav__selection' />
             </li>
-            <li className="li__dropdown">
-              <div className="dropdown">
-                <button className="dropbtn">
-                  Web-Site <i className="fas fa-angle-down nav__chevron"></i>
+            <li className='li__dropdown'>
+              <div className='dropdown'>
+                <button className='dropbtn'>
+                  Web-Site <i className='fas fa-angle-down nav__chevron'></i>
                 </button>
-                <div className="dropdown-content">
-                  <Link to="/">
-                    <button className="link">Home</button>
+                <div className='dropdown-content'>
+                  <Link to='/'>
+                    <button className='link'>Home</button>
                   </Link>
-                  <Link to="gourmet-catering">
-                    <button className="link">Gourmet Catering</button>
+                  <Link to='gourmet-catering'>
+                    <button className='link'>Gourmet Catering</button>
                   </Link>
                 </div>
               </div>
             </li>
           </ul>
         </nav>
-        <main className="main__content">
+        <main className='main__content'>
           <Switch>
-            <Route path="/gourmet-catering">
+            <Route path='/gourmet-catering'>
               <GourmetCatering />
             </Route>
-            <Route path="/">
+            <Route path='/'>
               <Home />
             </Route>
           </Switch>
