5af50e35c24c5e0d1d41973feb1456e1f4b51f0c
Julio <july2m3@gmail.com>
2020-07-23 16:06:30 -0500

    Added website info to homepage

--- src/components/home/Body.tsx
+++ src/components/home/Body.tsx
@@ -4,7 +4,13 @@ function Body() {
   return (
     <>
       <main className="home__main">
-        <p>This is my portfolio of example websites</p>
+        <h3>This is my portfolio of example websites</h3>
+        <div className="websites">
+          <ul>
+            <li>Gourmet Catering: A vegan catering restaurant.</li>
+            <li>LoneSome Design: A photography and media LLC.</li>
+          </ul>
+        </div>
       </main>
     </>
   );
